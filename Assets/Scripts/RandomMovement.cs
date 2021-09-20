726d0fd6a72e35cb9e8835845aa443035d68badb
kneuroth <34626694+kneuroth@users.noreply.github.com>
2021-09-20 11:37:36 -0400

    Added random movement to snacks and drag and drop

--- Assets/Scripts/RandomMovement.cs
+++ Assets/Scripts/RandomMovement.cs
@@ -1,17 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RandomMovement : MonoBehaviour
 {
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    Vector2 targetPosition;
+
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetPosition = GetRandomPosition();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if((Vector2)transform.position != targetPosition)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        } else
+        {
+            targetPosition = GetRandomPosition();
+        }
+    }
+
+    Vector2 GetRandomPosition()
+    {
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxY);
+        return new Vector2(randomX, randomY);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         
     }
