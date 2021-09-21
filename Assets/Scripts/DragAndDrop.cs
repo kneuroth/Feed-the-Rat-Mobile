a4d99ea6eef8bd994d7ecfa58b93224dfccf247e
kneuroth <34626694+kneuroth@users.noreply.github.com>
2021-09-21 19:45:47 -0400

    Added variable drag and drop script

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class DragAndDrop : MonoBehaviour
 {
+    public bool active;
     bool moveAllowed;
     Collider2D col;
     // Start is called before the first frame update
@@ -15,31 +16,39 @@ public class DragAndDrop : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.touchCount > 0)
+        if (active)
         {
-            Touch touch = Input.GetTouch(0);
-            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
 
-            if(touch.phase == TouchPhase.Began)
-            {
-                Collider2D touchedCollider = Physics2D.OverlapPoint(touchPosition);
-                if(col == touchedCollider)
+                if (touch.phase == TouchPhase.Began)
                 {
-                    moveAllowed = true;
+                    Collider2D touchedCollider = Physics2D.OverlapPoint(touchPosition);
+                    if (col == touchedCollider)
+                    {
+                        moveAllowed = true;
+                    }
                 }
-            }
-            if(touch.phase == TouchPhase.Moved)
-            {
-                if(moveAllowed == true)
+                if (touch.phase == TouchPhase.Moved)
                 {
-                    transform.position = new Vector2(touchPosition.x, touchPosition.y);
+                    if (moveAllowed == true)
+                    {
+                        transform.position = new Vector2(touchPosition.x, touchPosition.y);
+                    }
                 }
-            }
-            if(touch.phase == TouchPhase.Ended)
-            {
-                moveAllowed = false;
+                if (touch.phase == TouchPhase.Ended)
+                {
+                    moveAllowed = false;
+                }
             }
         }
 
+    }
+
+    void setMoveAllowed(bool allow)
+    {
+        moveAllowed = allow;
     }
 }
---
+++ Assets/Scripts/VariableDragAndDrop.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableDragAndDrop : MonoBehaviour
+{
+    
+    // How often rat will "try" to be draggable (0-1)
+    public float draggableRarity; 
+
+    // How likely that the rat will become draggable (0-1)
+    public float draggableProbability;
+
+    // How many seconds the rat is draggable for (-1 for random time between 1 and 5 seconds)
+    public int draggableDuration;
+
+    int draggableTickdown;
+
+    private bool draggable;
+
+    DragAndDrop dragAndDrop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        setDraggableTickdown();
+        dragAndDrop = GetComponent<DragAndDrop>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if this instance of the rat is draggable..
+        if (draggableRarity > 0)
+        {
+            // Try draggable probability after draggable tickdown is up and then reset the tickdown
+            if (draggableTickdown < 0)
+            {
+                rollDraggable();
+                setDraggableTickdown();
+            }
+        }
+
+        if (draggable)
+        {
+            // TODO show draggable sprite
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.color = Color.red;
+        } else
+        {
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.color = Color.white;
+            // TODO show non draggable sprite(s)
+        }
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (!draggable)
+        {
+            draggableTickdown--;
+        }
+    }
+
+    IEnumerator Draggable()
+    {
+        draggable = true;
+        dragAndDrop.active = true;
+        yield return new WaitForSeconds(draggableDuration == -1 ? Random.Range(1, 5) : draggableDuration);
+        draggable = false;
+        dragAndDrop.active = false;
+    }
+
+    private void setDraggableTickdown()
+    {
+        // tickdown is inverse of rarity factor * 1000
+        draggableTickdown = (int) (1/draggableRarity * 100);
+    }
+
+    private void rollDraggable()
+    {
+        // try random function 
+        if(Random.value <= draggableProbability)
+        {
+            // Success
+            StartCoroutine(Draggable());
+        }
+    }
+}
