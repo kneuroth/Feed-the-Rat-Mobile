8b4ed60736585e3b336ea462109867ab2c5f8d65
kneuroth <34626694+kneuroth@users.noreply.github.com>
2021-09-21 22:34:00 -0400

    Made timer and UI panels

--- Assets/Scripts/LevelTimer.cs
+++ Assets/Scripts/LevelTimer.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class LevelTimer : MonoBehaviour
 {
+    
+    public TextMeshProUGUI timerText;
+    private float startTime;
     // Start is called before the first frame update
+ 
+
     void Start()
     {
-        
+        startTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float t = Time.time - startTime;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("00.00");
+        timerText.text = minutes + ":" + seconds;
     }
 }
